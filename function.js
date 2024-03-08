5455c80e3e4815fef174ecbcf3b8b0b454cf7a0d
Juan Rozas <juanbr.rozas2005@gmail.com>
2024-03-08 12:17:47 -0300

    segundo commit

--- function.js
+++ function.js
@@ -1,6 +1,27 @@
-function Button_fixed(){
-    let Button_fixed = document.getElementById("button-fixed");
-    let button_romeve = document.getElementById("button-remove");
+var my_skills = document.getElementsByClassName("my-skills")[0];
+var my_skills1 = document.getElementsByClassName("my-skills")[1];
+var my_skills2 = document.getElementsByClassName("my-skills")[2];
 
-    if ()
+var back_github = document.getElementById("back-github");
+
+var back_git = document.getElementById("back-git");
+
+
+function Arrow_left(){  
+
+    back_github.style.display = "flex";
+    back_git.style.display = "flex"
+
+    my_skills.style.display = "none"
+    my_skills1.style.display = "none";
+    my_skills2.style.display = "none";
+}
+
+function Arrow_rigth(){
+    back_github.style.display = "none";
+    back_git.style.display = "none"
+
+    my_skills.style.display = "flex";
+    my_skills1.style.display = "flex";
+    my_skills2.style.display = "flex";
 }
--- scroll.js
+++ scroll.js
@@ -1,5 +1,8 @@
 window.addEventListener("scroll", function(){
-    let header = document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 0)
+    var header = document.querySelector('#header')
+    header.classList.toggle('rolagem', window.scrollY > 0 )
 })
+
+
+
 
