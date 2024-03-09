bb82699c0f61c731db6ce1e4d9bf38974efbd263
Juan Rozas <juanbr.rozas2005@gmail.com>
2024-03-09 10:49:03 -0300

    terceiro commit

--- scroll.js
+++ scroll.js
@@ -1,7 +1,11 @@
 window.addEventListener("scroll", function(){
     var header = document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 0 )
+    header.classList.toggle('rolagem', window.scrollY > 0  )
 })
+
+
+
+
 
 
 
